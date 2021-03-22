f65f020c3a69cdcb17e7a8bd199ffa48f6ef760a
anjun77 <anjunshao93@gmail.com>
2021-03-21 22:18:17 -0700

    Add Widget model, controller and service

---
+++ src/main/java/com/example/wbdvsp2101anjunshaoserverjava/controllers/WidgetController.java
@@ -0,0 +1,59 @@
+package com.example.wbdvsp2101anjunshaoserverjava.controllers;
+
+import com.example.wbdvsp2101anjunshaoserverjava.models.Widget;
+import com.example.wbdvsp2101anjunshaoserverjava.services.WidgetService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+  @Autowired
+  WidgetService service;// = new WidgetService();
+
+  @PostMapping("/api/topics/{tid}/widgets")
+  public Widget createWidgetForTopic(
+      @PathVariable("tid") String topicId,
+      @RequestBody Widget widget) {
+    widget.setTopicId(topicId);
+    return service.createWidget(widget);
+  }
+
+  @GetMapping("/api/topics/{tid}/widgets")
+  public List<Widget> findWidgetsForTopic(
+      @PathVariable("tid") String topicId
+  ) {
+    return service.findWidgetsForTopic(topicId);
+  }
+
+  @GetMapping("/api/widgets")
+  public List<Widget> findAllWidgets() {
+    return service.findAllWidgets();
+  }
+
+  @GetMapping("/api/widgets/{wid}")
+  public Widget findWidgetById(
+      @PathVariable("wid") Long id) {
+    return service.findWidgetById(id);
+  }
+
+  @DeleteMapping("/api/widgets/{wid}")
+  public Integer deleteWidget(@PathVariable("wid") Long id) {
+    return service.deleteWidget(id);
+  }
+
+  @PutMapping("/api/widgets/{wid}")
+  public Integer updateWidget(
+      @PathVariable("wid") Long id,
+      @RequestBody Widget widget) {
+    return service.updateWidget(id, widget);
+  }
+}
